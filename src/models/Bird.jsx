129769d2b9aa5e5d9a2be286681d0c2dc7544049
ArthemisCOF <Sorawit.Ily@hotmail.com>
2024-05-31 20:12:30 +0700

    add models

---
+++ src/models/Bird.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import birdScene from '../assets/3d/bird.glb';
+
+const Bird = () => {
+  return (
+    <mesh>
+
+    </mesh>
+  )
+}
+
+export default Bird
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 
-import  Island  from "../models/Island"
-import { Sky } from "@react-three/drei";
+import  Island  from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+
 
 {
   /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -38,12 +41,14 @@ const Home = () => {
             <ambientLight intensity={0.5}/>
             <hemisphereLight skyColor="#b1e1ff" groundColor="000000" intensity={1}/>
             
-            <Sky/>
+            <Bird/>
+            <Sky />
             <Island 
                 position={islandPosition}
                 scale={islandScale}
                 rotation={islandRotation}
             />
+            <Plane />
         </Suspense>
       </Canvas>
     </section>
